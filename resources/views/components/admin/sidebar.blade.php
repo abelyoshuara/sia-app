1f8e4ef850b48a0e252ef39912fe25896445d666
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-01 04:16:16 +0800

    update sidebar component

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -45,8 +45,7 @@
       </li>
 
       <li class="hs-accordion" id="projects-accordion">
-        <button type="button"
-          class="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent text-sm text-neutral-700 rounded-lg hover:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-300 dark:hs-accordion-active:text-white">
+        <x-admin.sidebar.dropdown-item :active="request()->is('akun*')">
           <svg class="flex-shrink-0 mt-0.5 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
             stroke-linejoin="round">
@@ -63,40 +62,25 @@
             <path d="m20.7 13.8 1-.4" />
           </svg>
           Kode Akun
-
-          <svg class="hs-accordion-active:block ms-auto hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-            stroke-linecap="round" stroke-linejoin="round">
-            <path d="m18 15-6-6-6 6" />
-          </svg>
-
-          <svg class="hs-accordion-active:hidden ms-auto block size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-            stroke-linecap="round" stroke-linejoin="round">
-            <path d="m6 9 6 6 6-6" />
-          </svg>
-        </button>
+        </x-admin.sidebar.dropdown-item>
 
         <div id="projects-accordion-child"
           class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden">
           <ul class="pt-2 ps-2">
             <li>
-              <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-neutral-700 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-700 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
-                href="{{ route('akun1.index') }}">
+              <x-admin.sidebar.link href="{{ route('akun1.index') }}">
                 Akun 1
-              </a>
+              </x-admin.sidebar.link>
             </li>
             <li>
-              <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-neutral-700 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-700 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
-                href="{{ route('dashboard') }}">
+              <x-admin.sidebar.link href="/akun2">
                 Akun 2
-              </a>
+              </x-admin.sidebar.link>
             </li>
             <li>
-              <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-neutral-700 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-700 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
-                href="{{ route('dashboard') }}">
+              <x-admin.sidebar.link href="/akun3">
                 Akun 3
-              </a>
+              </x-admin.sidebar.link>
             </li>
           </ul>
         </div>
