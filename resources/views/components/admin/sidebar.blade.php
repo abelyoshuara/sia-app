ee71adc6d503b725c8d3735e463318f5d6526ae5
Abel Yoshuara <abelyoshuara@gmail.com>
2024-04-30 20:53:05 +0800

    update sidebar component

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -83,7 +83,7 @@
           <ul class="pt-2 ps-2">
             <li>
               <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-neutral-700 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-700 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
-                href="{{ route('dashboard') }}">
+                href="{{ route('akun1.index') }}">
                 Akun 1
               </a>
             </li>
