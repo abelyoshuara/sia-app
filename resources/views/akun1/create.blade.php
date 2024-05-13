f22b64d016083d00c45188e5704d133ec33ce4f4
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-14 07:02:46 +0800

    refactor create view

--- resources/views/akun1/create.blade.php
+++ resources/views/akun1/create.blade.php
@@ -46,10 +46,7 @@
                     class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
                     Cancel
                   </a>
-                  <button type="submit"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-                    Save
-                  </button>
+                  <x-admin.forms.button type="submit">Save</x-admin.forms.button>
                 </div>
               </div>
             </form>
