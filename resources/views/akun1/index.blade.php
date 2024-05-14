bbc2f4813762d0b270a3770781808884bf50f9a6
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-14 22:02:38 +0800

    refactor list view

--- resources/views/akun1/index.blade.php
+++ resources/views/akun1/index.blade.php
@@ -1,40 +1,17 @@
 <x-layouts.admin title="Akun 1">
-  <!-- Card -->
   <div class="flex flex-col">
     <div class="-m-1.5 overflow-x-auto">
       <div class="p-1.5 min-w-full inline-block align-middle">
-        <div
-          class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-neutral-800 dark:border-neutral-700">
-          <!-- Header -->
-          <div
-            class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-neutral-700">
-            <div>
-              <h2 class="text-xl font-semibold text-gray-800 dark:text-neutral-200">
-                Kode Akun
-              </h2>
-              <p class="text-sm text-gray-600 dark:text-neutral-400">
-                Daftar Akun 1
-              </p>
-            </div>
 
-            <div>
-              <div class="inline-flex gap-x-2">
-                <a class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-                  href="{{ route('akun1.create') }}">
-                  <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path d="M5 12h14" />
-                    <path d="M12 5v14" />
-                  </svg>
-                  Tambah Akun 1
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- End Header -->
+        <x-admin.card>
+          <x-admin.card.header>
+            <x-slot:title>Akun 1</x-slot>
+            <x-slot:subtitle>Daftar Akun 1</x-slot>
+            <x-slot:actions>
+              <x-admin.button as="a" href="{{ route('akun1.create') }}" size="sm">Buat Akun 1</x-admin.button>
+            </x-slot>
+          </x-admin.card.header>
 
-          <!-- Table -->
           <x-admin.table>
             <x-admin.table.thead>
               <tr>
@@ -105,11 +82,8 @@
               @endforeach
             </x-admin.table.tbody>
           </x-admin.table>
-          <!-- End Table -->
 
-          <!-- Footer -->
-          <div
-            class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
+          <x-admin.card.footer>
             <div>
               <p class="text-sm text-gray-600 dark:text-neutral-400">
                 <span class="font-semibold text-gray-800 dark:text-neutral-200">12</span> results
@@ -139,11 +113,10 @@
                 </button>
               </div>
             </div>
-          </div>
-          <!-- End Footer -->
-        </div>
+          </x-admin.card.footer>
+        </x-admin.card>
+
       </div>
     </div>
   </div>
-  <!-- End Card -->
 </x-layouts.admin>
