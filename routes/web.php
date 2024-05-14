75ffed8069b63b6e49e2b23407cfd81c1640511c
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-14 22:00:36 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\Akun1Controller;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -16,8 +12,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('akun1', Akun1Controller::class);
 });
 
-Route::resource('akun1', Akun1Controller::class);
 
 require __DIR__ . '/auth.php';
