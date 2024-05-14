4259418cce5af46dab2905f520a22e220038c3ea
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-15 05:43:41 +0800

    add a edit feature

--- app/Http/Controllers/Akun1Controller.php
+++ app/Http/Controllers/Akun1Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreAkun1Request;
+use App\Http\Requests\UpdateAkun1Request;
 use App\Models\Akun1;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -48,17 +49,19 @@ class Akun1Controller extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Akun1 $akun1): View
     {
-        //
+        return view('akun1.edit', compact('akun1'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateAkun1Request $request, Akun1 $akun1)
     {
-        //
+        $akun1->update($request->validated());
+
+        return to_route('akun1.index')->with('success', 'Akun 1 updated successfully.');
     }
 
     /**
---
+++ resources/views/akun1/edit.blade.php
@@ -0,0 +1,39 @@
+<x-layouts.admin title="Akun 1">
+  <x-admin.card>
+    <x-admin.card.header>
+      <x-slot:title>Akun 1</x-slot>
+      <x-slot:subtitle>Edit Akun 1</x-slot>
+    </x-admin.card.header>
+
+    <x-admin.card.content>
+      <form action="{{ route('akun1.update', $akun1->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="space-y-3">
+          <div class="space-y-2">
+            <x-admin.forms.input-label for="kode"
+              class="inline-block text-sm font-medium text-gray-800 dark:text-neutral-200" :value="__('Kode')" />
+            <x-admin.forms.text-input id="kode" name="kode" :value="old('kode', $akun1->kode)" placeholder="Masukan Kode"
+              :error="$errors->first('kode')" />
+            <x-admin.forms.input-error :message="$errors->first('kode')" />
+          </div>
+
+          <div class="space-y-2">
+            <x-admin.forms.input-label for="nama"
+              class="inline-block text-sm font-medium text-gray-800 dark:text-neutral-200" :value="__('Nama')" />
+            <x-admin.forms.text-input id="nama" name="nama" :value="old('nama', $akun1->nama)" placeholder="Masukan Nama"
+              :error="$errors->first('nama')" />
+            <x-admin.forms.input-error :message="$errors->first('nama')" />
+          </div>
+
+          <div class="flex justify-end gap-x-2 pt-4">
+            <x-admin.button as="a" href="{{ route('akun1.index') }}" variant="white"
+              color="dark">Cancel</x-admin.button>
+            <x-admin.button type="submit">Update</x-admin.button>
+          </div>
+        </div>
+      </form>
+    </x-admin.card.content>
+  </x-admin.card>
+</x-layouts.admin>
