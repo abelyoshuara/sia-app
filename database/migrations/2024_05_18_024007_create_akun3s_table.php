8537a16fe05be0368d2a0dbeb5c5d72e3c0c69fd
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-18 10:57:03 +0800

    add a akun3 table

---
+++ database/migrations/2024_05_18_024007_create_akun3s_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('akun3s', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->string('nama');
+            $table->unsignedBigInteger('akun1_id');
+            $table->unsignedBigInteger('akun2_id');
+            $table->timestamps();
+
+            $table->foreign('akun1_id')->references('id')->on('akun1s');
+            $table->foreign('akun2_id')->references('id')->on('akun2s');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('akun3s', function (Blueprint $table) {
+            $table->dropForeign(['akun1_id', 'akun2_id']);
+        });
+
+        Schema::dropIfExists('akun3s');
+    }
+};
