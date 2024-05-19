66c860ca52f6b3c96e58d6eaa4049c95dd48c629
Abel Yoshuara <abelyoshuara@gmail.com>
2024-05-19 14:06:16 +0800

    update a text-input component

--- resources/views/components/admin/forms/text-input.blade.php
+++ resources/views/components/admin/forms/text-input.blade.php
@@ -4,7 +4,7 @@
   $classes =
       $error ?? false
           ? 'py-3 px-4 block w-full border-red-500 rounded-lg text-sm focus:border-red-500 focus:ring-red-500 dark:bg-neutral-800 dark:border-red-500 dark:text-neutral-400'
-          : 'py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600';
+          : 'py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-blue-500';
 @endphp
 
 <div class="relative">
