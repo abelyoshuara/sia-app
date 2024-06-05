b8b09ee3d9a31fc96664a417f78def250251587d
Abel Yoshuara <abelyoshuara@gmail.com>
2024-06-05 21:25:04 +0800

    update table component

--- resources/views/components/admin/table/index.blade.php
+++ resources/views/components/admin/table/index.blade.php
@@ -1,3 +1,3 @@
-<table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+<table {{ $attributes->merge(['class' => 'min-w-full divide-y divide-gray-200 dark:divide-neutral-700']) }}>
   {{ $slot }}
 </table>
