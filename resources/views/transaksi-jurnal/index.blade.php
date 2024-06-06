a3c969ebf6f360dc8ef85777e42026859d578c07
Abel Yoshuara <abelyoshuara@gmail.com>
2024-06-06 17:13:07 +0800

    add a detail feature

--- resources/views/transaksi-jurnal/index.blade.php
+++ resources/views/transaksi-jurnal/index.blade.php
@@ -1,4 +1,100 @@
 <x-layouts.admin title="Transaksi Jurnal">
+  <x-slot:scripts>
+    <script>
+      const table = document.querySelector('#table-transaksi-jurnal');
+      const tabelNilaiTransaksiJurnal = document.querySelector('#detail-modal #tabel-detail-nilai-transaksi-jurnal');
+      const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+      const renderTableNilaiTransaksiJurnal = (data) => {
+        let html = '';
+
+        if (data.length > 0) {
+          data.forEach((n, i) => {
+            html += `
+              <tr>
+                <x-admin.table.td>
+                  <div class="px-6 py-3">
+                    <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">${i+1}</span>
+                  </div>
+                </x-admin.table.td>
+
+                <x-admin.table.td class="h-px w-72">
+                  <div class="px-6 py-3">
+                    <span class="text-sm text-gray-500 dark:text-neutral-500">${n.akun3.nama}</span>
+                  </div>
+                </x-admin.table.td>
+
+                <x-admin.table.td class="h-px w-72">
+                  <div class="px-6 py-3">
+                    <span class="text-sm text-gray-500 dark:text-neutral-500">${n.debit}</span>
+                  </div>
+                </x-admin.table.td>
+
+                <x-admin.table.td class="h-px w-72">
+                  <div class="px-6 py-3">
+                    <span class="text-sm text-gray-500 dark:text-neutral-500">${n.kredit}</span>
+                  </div>
+                </x-admin.table.td>
+
+                <x-admin.table.td>
+                  <div class="px-6 py-3">
+                    <span class="text-sm text-gray-500 dark:text-neutral-500">${n.status.nama}</span>
+                  </div>
+                </x-admin.table.td>
+              </tr>`;
+          });
+        } else {
+          html += `<tr>
+                    <x-admin.table.td class="h-px w-72 text-center" colspan="6">
+                      <div class="px-6 py-3">
+                        <span class="text-sm text-gray-500 dark:text-neutral-500">Data belum tersedia</span>
+                      </div>
+                    </x-admin.table.td>
+                  </tr>`;
+        }
+
+        tabelNilaiTransaksiJurnal.querySelector('tbody').innerHTML = html;
+      }
+
+      const fetchTransaksiJurnalById = async (id) => {
+        try {
+          const response = await fetch(`/transaksi-jurnal/${id}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-TOKEN': csrfToken
+            }
+          });
+
+          const responseJson = await response.json();
+          const {
+            kwitansi,
+            tanggal,
+            deskripsi,
+            keterangan_jurnal,
+            nilai_jurnal: nilai
+          } = responseJson.data;
+
+          document.querySelector('#kwitansi').textContent = kwitansi;
+          document.querySelector('#tanggal').textContent = tanggal;
+          document.querySelector('#deskripsi').textContent = deskripsi;
+          document.querySelector('#keterangan_jurnal').textContent = keterangan_jurnal;
+
+          renderTableNilaiTransaksiJurnal(nilai);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      table.addEventListener('click', (event) => {
+        if (event.target.classList.contains('button-detail')) {
+          const id = event.target.dataset.id;
+          fetchTransaksiJurnalById(id);
+        }
+      });
+    </script>
+  </x-slot>
+
   <div class="flex flex-col">
     <div class="-m-1.5 overflow-x-auto">
       <div class="p-1.5 min-w-full inline-block align-middle">
@@ -13,7 +109,7 @@
             </x-slot>
           </x-admin.card.header>
 
-          <x-admin.table>
+          <x-admin.table id="table-transaksi-jurnal">
             <x-admin.table.thead>
               <tr>
                 <x-admin.table.th class="ps-6 text-start whitespace-nowrap">
@@ -97,8 +193,8 @@
 
                   <x-admin.table.td>
                     <div class="inline-flex">
-                      <x-admin.button as="a" href="{{ route('transaksi-jurnal.edit', $item->id) }}"
-                        variant="link" size="sm">
+                      <x-admin.button variant="link" size="sm" data-hs-overlay="#detail-modal"
+                        data-id="{{ $item->id }}" class="button-detail">
                         Detail
                       </x-admin.button>
                       <x-admin.button as="a" href="{{ route('transaksi-jurnal.edit', $item->id) }}"
@@ -154,6 +250,168 @@
             </x-slot:pagination>
           </x-admin.card.footer>
         </x-admin.card>
+      </div>
+    </div>
+  </div>
+
+  <div id="detail-modal"
+    class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none">
+    <div
+      class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all lg:max-w-4xl lg:w-full m-3 lg:mx-auto">
+      <div
+        class="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
+        <div class="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
+          <h3 class="font-bold text-gray-800 dark:text-white">
+            Detail Transaksi Jurnal
+          </h3>
+          <button type="button"
+            class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700"
+            data-hs-overlay="#detail-modal">
+            <span class="sr-only">Close</span>
+            <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+              viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+              stroke-linejoin="round">
+              <path d="M18 6 6 18"></path>
+              <path d="m6 6 12 12"></path>
+            </svg>
+          </button>
+        </div>
+        <div class="p-4 overflow-y-auto flex flex-col gap-y-4">
+
+          <div class="-m-1.5 overflow-x-auto">
+            <div class="p-1.5 min-w-full inline-block align-middle">
+              <div class="border rounded-lg dark:border-neutral-700">
+                <x-admin.table id="tabel-detail-transaksi-jurnal">
+                  <x-admin.table.tbody>
+
+                    <tr>
+                      <x-admin.table.td class="w-1/5">
+                        <div class="px-6 py-3">
+                          <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">Kwitansi</span>
+                        </div>
+                      </x-admin.table.td>
+                      <x-admin.table.td class="h-px w-72">
+                        <div class="px-6 py-3">
+                          <span class="text-sm text-gray-500 dark:text-neutral-500" id="kwitansi"></span>
+                        </div>
+                      </x-admin.table.td>
+                    </tr>
+
+                    <tr>
+                      <x-admin.table.td>
+                        <div class="px-6 py-3">
+                          <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">Tanggal</span>
+                        </div>
+                      </x-admin.table.td>
+                      <x-admin.table.td class="h-px w-72">
+                        <div class="px-6 py-3">
+                          <span class="text-sm text-gray-500 dark:text-neutral-500" id="tanggal"></span>
+                        </div>
+                      </x-admin.table.td>
+                    </tr>
+
+                    <tr>
+                      <x-admin.table.td>
+                        <div class="px-6 py-3">
+                          <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">Keterangan
+                            Jurnal</span>
+                        </div>
+                      </x-admin.table.td>
+                      <x-admin.table.td class="h-px w-72">
+                        <div class="px-6 py-3">
+                          <span class="text-sm text-gray-500 dark:text-neutral-500" id="keterangan_jurnal"></span>
+                        </div>
+                      </x-admin.table.td>
+                    </tr>
+
+                    <tr>
+                      <x-admin.table.td>
+                        <div class="px-6 py-3">
+                          <span class="text-sm font-semibold text-gray-800 dark:text-neutral-200">Deskripsi</span>
+                        </div>
+                      </x-admin.table.td>
+                      <x-admin.table.td class="h-px w-72">
+                        <div class="px-6 py-3">
+                          <span class="text-sm text-gray-500 dark:text-neutral-500" id="deskripsi"></span>
+                        </div>
+                      </x-admin.table.td>
+                    </tr>
+
+                  </x-admin.table.tbody>
+                </x-admin.table>
+              </div>
+            </div>
+          </div>
+
+          <div class="-m-1.5 overflow-x-auto">
+            <div class="p-1.5 min-w-full inline-block align-middle">
+              <div class="border rounded-lg dark:border-neutral-700">
+                <x-admin.table id="tabel-detail-nilai-transaksi-jurnal">
+                  <x-admin.table.thead>
+                    <tr>
+                      <x-admin.table.th class="ps-6 text-start whitespace-nowrap">
+                        <div class="flex items-center gap-x-2">
+                          <span
+                            class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                            No
+                          </span>
+                        </div>
+                      </x-admin.table.th>
+
+                      <x-admin.table.th class="px-6 text-start whitespace-nowrap">
+                        <div class="flex items-center gap-x-2">
+                          <span
+                            class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                            Kode Akun
+                          </span>
+                        </div>
+                      </x-admin.table.th>
+
+                      <x-admin.table.th class="px-6 text-start whitespace-nowrap">
+                        <div class="flex items-center gap-x-2">
+                          <span
+                            class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                            Debit
+                          </span>
+                        </div>
+                      </x-admin.table.th>
+
+                      <x-admin.table.th class="px-6 text-start whitespace-nowrap">
+                        <div class="flex items-center gap-x-2">
+                          <span
+                            class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                            Kredit
+                          </span>
+                        </div>
+                      </x-admin.table.th>
+
+                      <x-admin.table.th class="px-6 text-start whitespace-nowrap">
+                        <div class="flex items-center gap-x-2">
+                          <span
+                            class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                            Status
+                          </span>
+                        </div>
+                      </x-admin.table.th>
+
+                      <x-admin.table.th class="px-6 text-end"></x-admin.table.th>
+                    </tr>
+                  </x-admin.table.thead>
+
+                  <x-admin.table.tbody></x-admin.table.tbody>
+                </x-admin.table>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
+          <button type="button"
+            class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+            data-hs-overlay="#detail-modal">
+            Close
+          </button>
+        </div>
       </div>
     </div>
   </div>
