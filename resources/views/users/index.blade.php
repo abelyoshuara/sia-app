d1633e52fb2871b4684f6cd1730c34ca89002a31
Abel Yoshuara <abelyoshuara@gmail.com>
2024-06-08 12:36:43 +0800

    add a get all users feature

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,141 @@
+<x-layouts.admin title="Pengguna">
+  <div class="flex flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+      <div class="p-1.5 min-w-full inline-block align-middle">
+        <x-admin.card class="overflow-hidden">
+          <x-admin.card.header>
+            <x-slot:title>Pengguna</x-slot>
+            <x-slot:subtitle>Daftar Pengguna</x-slot>
+            <x-slot:actions>
+              <x-admin.button as="a" href="{{ route('users.create') }}" size="sm">
+                Tambah
+              </x-admin.button>
+            </x-slot>
+          </x-admin.card.header>
+
+          <x-admin.table id="table-users">
+            <x-admin.table.thead>
+              <tr>
+                <x-admin.table.th class="ps-6 text-start whitespace-nowrap">
+                  <div class="flex items-center gap-x-2">
+                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                      No
+                    </span>
+                  </div>
+                </x-admin.table.th>
+
+                <x-admin.table.th class="px-6 text-start whitespace-nowrap">
+                  <div class="flex items-center gap-x-2">
+                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                      Name
+                    </span>
+                  </div>
+                </x-admin.table.th>
+
+                <x-admin.table.th class="px-6 text-start whitespace-nowrap">
+                  <div class="flex items-center gap-x-2">
+                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                      Email
+                    </span>
+                  </div>
+                </x-admin.table.th>
+
+                <x-admin.table.th class="px-6 text-start whitespace-nowrap">
+                  <div class="flex items-center gap-x-2">
+                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                      Role
+                    </span>
+                  </div>
+                </x-admin.table.th>
+
+                <x-admin.table.th class="px-6 text-end"></x-admin.table.th>
+              </tr>
+            </x-admin.table.thead>
+
+            <x-admin.table.tbody>
+              @forelse ($users as $user)
+                <tr>
+                  <x-admin.table.td>
+                    <div class="px-6 py-3">
+                      <span
+                        class="text-sm font-semibold text-gray-800 dark:text-neutral-200">{{ $loop->iteration }}</span>
+                    </div>
+                  </x-admin.table.td>
+
+                  <x-admin.table.td class="h-px w-72">
+                    <div class="px-6 py-3">
+                      <span class="text-sm text-gray-500 dark:text-neutral-500">{{ $user->name }}</span>
+                    </div>
+                  </x-admin.table.td>
+
+                  <x-admin.table.td class="h-px w-72">
+                    <div class="px-6 py-3">
+                      <span class="text-sm text-gray-500 dark:text-neutral-500">{{ $user->email }}</span>
+                    </div>
+                  </x-admin.table.td>
+
+                  <x-admin.table.td>
+                    <div class="px-6 py-3">
+                      <span class="text-sm text-gray-500 dark:text-neutral-500">{{ $user->role }}</span>
+                    </div>
+                  </x-admin.table.td>
+
+                  <x-admin.table.td>
+                    <div class="inline-flex">
+                      <x-admin.button as="a" href="{{ route('users.edit', $user->id) }}" variant="link"
+                        size="sm">
+                        Ubah
+                      </x-admin.button>
+                      <form action="{{ route('users.destroy', $user->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <x-admin.button type="submit" variant="link" size="sm">
+                          Hapus
+                        </x-admin.button>
+                      </form>
+                    </div>
+                  </x-admin.table.td>
+                </tr>
+              @empty
+                <tr>
+                  <x-admin.table.td colspan="6">
+                    <div class="px-6 py-3 text-center">
+                      <span class="text-sm font-semibold text-red-500">Data belum tersedia!</span>
+                    </div>
+                  </x-admin.table.td>
+                </tr>
+              @endforelse
+            </x-admin.table.tbody>
+          </x-admin.table>
+
+          <x-admin.card.footer>
+            <x-slot:total>{{ count($users) }}</x-slot>
+            <x-slot:pagination>
+              <div class="inline-flex gap-x-2">
+                <button type="button"
+                  class="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                  <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path d="m15 18-6-6 6-6" />
+                  </svg>
+                  Prev
+                </button>
+
+                <button type="button"
+                  class="py-1.5 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                  Next
+                  <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path d="m9 18 6-6-6-6" />
+                  </svg>
+                </button>
+              </div>
+            </x-slot:pagination>
+          </x-admin.card.footer>
+        </x-admin.card>
+      </div>
+    </div>
+  </div>
+</x-layouts.admin>
