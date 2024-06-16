420e8d75cc9870a17cec33f3466db363812c86af
Abel Yoshuara <abelyoshuara@gmail.com>
2024-06-17 06:08:47 +0800

    update card.footer component

--- resources/views/components/admin/card/footer.blade.php
+++ resources/views/components/admin/card/footer.blade.php
@@ -1,10 +1,14 @@
-<div
-  {{ $attributes->merge(['class' => 'px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700']) }}>
-  <div>
-    <p class="text-sm text-gray-600 dark:text-neutral-400">
-      <span class="font-semibold text-gray-800 dark:text-neutral-200">{{ $total }}</span> results
-    </p>
-  </div>
+<div {{ $attributes->merge(['class' => 'px-6 py-4 border-t border-gray-200 dark:border-neutral-700']) }}>
+  {{ $slot }}
+
+  @if (isset($total))
+    <div>
+      <p class="text-sm text-gray-600 dark:text-neutral-400">
+        <span class="font-semibold text-gray-800 dark:text-neutral-200">{{ $total }}</span> results
+      </p>
+    </div>
+  @endif
+
   @if (isset($pagination))
     <div>
       {{ $pagination }}
