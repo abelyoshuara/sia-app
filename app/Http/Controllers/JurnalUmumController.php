7dbe797e0d3adf536aa0bc71fb2f8d5cbc2b41ae
Abel Yoshuara <abelyoshuara@gmail.com>
2024-06-17 16:46:40 +0800

    redesign jurnal-umum view

--- app/Http/Controllers/JurnalUmumController.php
+++ app/Http/Controllers/JurnalUmumController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransaksiJurnal;
 use Illuminate\Http\Request;
 
 class JurnalUmumController extends Controller
 {
     public function index()
     {
-        return view('jurnal-umum.index');
+        $jurnalUmum = TransaksiJurnal::paginate(10)->withQueryString();
+
+        return view('jurnal-umum.index', compact('jurnalUmum'));
     }
 }
