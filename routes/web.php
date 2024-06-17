bd49c5423ba6699a2050a52843c5497bb4604d78
Abel Yoshuara <abelyoshuara@gmail.com>
2024-06-17 14:32:58 +0800

    refactor dashboard route

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', fn () => view('dashboard'))->name('dashboard');
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
